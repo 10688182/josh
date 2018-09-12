ce19834094cebb03c9d5377552b421fdb61da49b
10688182 <42799155+10688182@users.noreply.github.com>
2018-09-12 17:41:53 +0000

    Update and rename GCD.cpp to A4_10688182.cc

---
+++ A4_10688182.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int gcd (int a, int b);
+
+   int main()
+{
+	int a, b;
+	
+	cout<<"Enter two positive integers:";
+	cin>> a >> b;
+	
+	cout<< "GCD of "   << a <<   " & "   << b <<  " is: " << gcd(a, b);
+	
+	return 0;	
+}
+	int gcd(int a, int b)
+{
+	if (b != 0)
+	
+		return gcd(b, a % b);
+
+	else
+		return a;
+		
+}
